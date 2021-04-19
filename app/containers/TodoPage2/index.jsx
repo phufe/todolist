95cc74146ae7d413ae1e38c7e063b582e80646e2
phufe <phunv@antsomi.com>
2021-04-19 16:51:45 +0700

    phá đảo todo -v2

--- app/containers/TodoPage2/index.jsx
+++ app/containers/TodoPage2/index.jsx
@@ -21,16 +21,11 @@ import TextField from '@material-ui/core/TextField';
 import H1 from 'components/H1';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
     backgroundColor: '#f5f5f5',
-  },
-  root2: {
-    width: "100%",
-    backgroundColor: theme.palette.background.transparent
   },
   title: {
     marginBottom: '1rem',
@@ -46,152 +41,157 @@ const useStyles = makeStyles(theme => ({
   },
   imgContainer: {
     display: 'block',
-    margin: 'auto'
+    margin: 'auto',
   },
 }));
 
-const URL = 'https://api-nodejs-todolist.herokuapp.com'
+const URL = 'https://api-nodejs-todolist.herokuapp.com';
 const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Zjc2ZTQzN2E2YTY3MzAwMTc0NzFjNmIiLCJpYXQiOjE2MDE2MjcxOTJ9.x6hiHZB6izKaoLB5RRKKeqX-J5TlqtFJMDu2NVtl5ak'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Zjc2ZTQzN2E2YTY3MzAwMTc0NzFjNmIiLCJpYXQiOjE2MDE2MjcxOTJ9.x6hiHZB6izKaoLB5RRKKeqX-J5TlqtFJMDu2NVtl5ak';
 const axiosConfig = {
   headers: {
-    Authorization: `${TOKEN}`
-  }
-}
+    Authorization: `${TOKEN}`,
+  },
+};
 
 export default function TodoPage2() {
-    const classes = useStyles();
-    const [tasks, setTasks] = useState([])
-    const [addTask, setAddTask] = useState('')
-    const [editMode, setEditMode] = useState({ isEdit: false, taskId: '' })
-    const [taskInput, setTaskInput] = useState('')
-    const [loading, setLoading] = useState(false)  
-    
-    useEffect(() => {
-        ;(async () => await fetchTasks())()
-    }, [])
-    
-    async function fetchTasks() {
-        try {
-            setLoading(true)
-            const { data } = await axios.get(`${URL}/task`, axiosConfig)
-            setTasks(data.data)
-        } catch (err) {
-            console.error('Error: ', err.message)
-        } finally {
-            setLoading(false)
-        }
-    }
+  const classes = useStyles();
+  const [tasks, setTasks] = useState([]);
+  const [addTask, setAddTask] = useState('');
+  const [editMode, setEditMode] = useState({ isEdit: false, taskId: '' });
+  const [taskInput, setTaskInput] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    async function handleCreateTask(e) {
-        e.preventDefault()
-        e.target.reset()
-        try {
-            setLoading(true)
-            await axios.post(`${URL}/task`, axiosConfig)
-            await fetchTasks()
-        } catch (err) {
-            console.error('Error: ', err.message)
-        } finally {
-            setLoading(false)
-        }
-    }
-    
-    async function handleEditTask(e, taskId) {
-        try {
-            setLoading(true)
-            await axios.put(
-              `${URL}/task/${taskId}`,
-              { description: taskInput },
-              axiosConfig
-            )
-            setEditMode({ isEdit: false, taskId: '' })
-            await fetchTasks()
-        } catch (err) {
-            console.error('Error: ', err.message)
-        } finally {
-            setLoading(false)
-        }    
-    }
-    
-    
-    async function handleDeleteTask(e, taskId, taskName) {
-        e.preventDefault()
-        const isConfirmed = window.confirm(`Are you sure to delete "${taskName}" ?`)
-        if (isConfirmed) {
-            try {
-                setLoading(true)
-                await axios.delete(`${URL}/task/${taskId}`, axiosConfig)
-                await fetchTasks()
-            } catch (err) {
-                console.error('Error:', err.message)
-            } finally {
-                setLoading(false)
-            }
-        }
-    }
-      
-    return (
-        <div>
-            <Helmet>
-                <title>Todo List 2</title>
-                <meta
-                name="description"
-                content="Add page of React.js Boilerplate application"
-                />
-            </Helmet>
+  useEffect(() => {
+    (async () => await fetchTasks())();
+  }, []);
 
-            <H1 className={classes.title}>
-                <FormattedMessage {...messages.header} />
-            </H1>
+  async function fetchTasks() {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`${URL}/task`, axiosConfig);
+      setTasks(data.data);
+    } catch (err) {
+      console.error('Error: ', err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-            <Container maxWidth="md">
-                <AppBar position="static">
-                    <Toolbar variant="dense" className={classes.subtitle}>
-                        <Typography variant="h6" color="inherit">
-                            React Todo List
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+  async function handleCreateTask(e) {
+    e.preventDefault();
+    e.target.reset();
+    try {
+      setLoading(true);
+      await axios.post(`${URL}/task`, {description: taskInput}, axiosConfig);
+      await fetchTasks();
+    } catch (err) {
+      console.error('Error: ', err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-                <div className={classes.root}>
-                    <Grid container spacing={2} className={classes.container}>
-                        <Grid item xs={12}>
-                            <form onSubmit={handleCreateTask}>
-                                <Grid container justify="center" alignItems="center">
-                                    <TextField
-                                        id="standard-basic"
-                                        label="Add Task"
-                                        variant="outlined"
-                                        size="small"
-                                    />
-                                </Grid>
-                            </form>
-                        </Grid>
-                        {loading ? (
-                            <div className={classes.imgContainer}>
-                                <img src="https://s4.gifyu.com/images/dinod9b5f2db539f2a9c.gif" className={classes.img}/>
-                            </div>
-                        ) : (
-                            <Grid item xs={12}>
-                                {tasks.map((task) => {
-                                    return (
-                                        <ListTodo2 
-                                            key={task._id}
-                                            todo={task}
-                                            editMode={editMode}
-                                            onDeleteTask={handleDeleteTask}
-                                            onEditTask={handleEditTask}
-                                            taskInput={taskInput}
-                                            setTaskInput={setTaskInput}            
-                                        />
-                                    )
-                                })}
-                            </Grid>
-                        )}
-                    </Grid>
-                </div>
-            </Container>
-        </div>
+  async function handleEditTask(e, taskId) {
+    try {
+      setLoading(true);
+      await axios.put(
+        `${URL}/task/${taskId}`,
+        { description: taskInput },
+        axiosConfig,
+      );
+      setEditMode({ isEdit: false, taskId: '' });
+      await fetchTasks();
+    } catch (err) {
+      console.error('Error: ', err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleDeleteTask(e, taskId, taskName) {
+    e.preventDefault();
+    const isConfirmed = window.confirm(
+      `Are you sure to delete "${taskName}" ?`,
     );
+    if (isConfirmed) {
+      try {
+        setLoading(true);
+        await axios.delete(`${URL}/task/${taskId}`, axiosConfig);
+        await fetchTasks();
+      } catch (err) {
+        console.error('Error:', err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Helmet>
+        <title>Todo List 2</title>
+        <meta
+          name="description"
+          content="Add page of React.js Boilerplate application"
+        />
+      </Helmet>
+
+      <H1 className={classes.title}>
+        <FormattedMessage {...messages.header} />
+      </H1>
+
+      <Container maxWidth="md">
+        <AppBar position="static">
+          <Toolbar variant="dense" className={classes.subtitle}>
+            <Typography variant="h6" color="inherit">
+              React Todo List ({tasks.length})
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.root}>
+          <Grid container spacing={2} className={classes.container}>
+            <Grid item xs={12}>
+              <form onSubmit={handleCreateTask}>
+                <Grid container justify="center" alignItems="center">
+                  <TextField
+                    id="standard-basic"
+                    label="Add Task"
+                    variant="outlined"
+                    size="small"
+                    onChange={(e) => setTaskInput(e.target.value)}
+                  />
+                </Grid>
+              </form>
+            </Grid>
+            {loading ? (
+              <div className={classes.imgContainer}>
+                <img
+                  src="https://s4.gifyu.com/images/dinod9b5f2db539f2a9c.gif"
+                  className={classes.img}
+                />
+              </div>
+            ) : (
+              <Grid item xs={12}>
+                {tasks.map(task => {
+                  return (
+                    <ListTodo2
+                      key={task._id}
+                      todo={task}
+                      editMode={editMode}
+                      onDeleteTask={handleDeleteTask}
+                      onEditTask={handleEditTask}
+                      taskInput={taskInput}
+                      setTaskInput={setTaskInput}
+                    />
+                  );
+                })}
+              </Grid>
+            )}
+          </Grid>
+        </div>
+      </Container>
+    </div>
+  );
 }
